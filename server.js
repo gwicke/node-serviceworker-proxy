d02c62cf0dd5e6972313640d408d6a645de33aae
Gabriel Wicke <gwicke@wikimedia.org>
2016-09-30 15:27:59 -0700

    Use bluebird as the global Promise.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 "use strict";
 
+// Improve performance
+global.Promise = require('bluebird');
+
 // B/C wrapper to make the old init script work with service-runner.
 var ServiceRunner = require('service-runner');
 
